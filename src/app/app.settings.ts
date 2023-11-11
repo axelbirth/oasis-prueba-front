d594c517d9413f9ecabfd7253896adfe1d5959be
axelml99 <axel@birth.com.mx>
2023-11-10 23:27:23 -0600

    update

---
+++ src/app/app.settings.ts
@@ -0,0 +1,3 @@
+export class AppSettings {
+  public static API_ENDPOINT = 'https://oasis-prueba-backend.onrender.com/api/v1/oasis/';
+}
--- src/app/services/oasis.service.ts
+++ src/app/services/oasis.service.ts
@@ -2,46 +2,46 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { OasisModel } from '../models/oasis.model';
+import { AppSettings } from '../app.settings';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OasisService {
-  URL = 'https://oasis-prueba-backend.onrender.com/api/v1/oasis/';
 
   constructor(private http: HttpClient) { }
 
   // Obtener todos los oasis
   getOasis(): Observable<any> {
-    return this.http.get(this.URL).pipe(map((res: any) => {
+    return this.http.get(`${AppSettings.API_ENDPOINT}`).pipe(map((res: any) => {
       return res;
     }));
   }
 
   //Obtener oasis destacados
   getOasisDestacados(): Observable<any> {
-    return this.http.get(this.URL + 'destacados/' + true).pipe(map((res: any) => {
+    return this.http.get(`${AppSettings.API_ENDPOINT}destacados/${true}`).pipe(map((res: any) => {
       return res;
     }));
   }
 
   //Añadir Oasis
   saveOasis(oasis: OasisModel): Observable<OasisModel> {
-    return this.http.post(this.URL, oasis).pipe(map((res: any) => {
+    return this.http.post(`${AppSettings.API_ENDPOINT}`, oasis).pipe(map((res: any) => {
       return res;
     }));
   }
 
   //Obtener oasis por agua
   getOasisByAgua(agua: number): Observable<any> {
-    return this.http.get(this.URL + 'agua/' + agua).pipe(map((res: any) => {
+    return this.http.get(`${AppSettings.API_ENDPOINT}agua/${agua}`).pipe(map((res: any) => {
       return res;
     }));
   }
 
   //Obtener oasis por latitud y longitud
   getOasisByLatLong(latitud: number, longitud: number): Observable<any> {
-    return this.http.get(this.URL + 'lat/' + latitud + '/long/' + longitud).pipe(map((res: any) => {
+    return this.http.get(`${AppSettings.API_ENDPOINT}lat/${latitud}/long/${longitud}`).pipe(map((res: any) => {
       return res;
     }));
   }
